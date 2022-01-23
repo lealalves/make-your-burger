864614c4e5546003dc3c50e755db9562f30fdc1d
lealalves <leealalves@hotmail.com>
2022-01-23 16:13:23 -0300

    estilizando navbar/footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>Footer</div>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2021</p>
+    </footer>
 </template>
 
 <script>
@@ -7,3 +9,17 @@ export default {
     name: "Footer"
 }
 </script>
+
+<style scoped>
+    #footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: wheat;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
