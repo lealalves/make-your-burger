e28e5b9543bca6871a444024de53420ab3d8c572
lealalves <leealalves@hotmail.com>
2022-01-23 22:18:58 -0300

    estilização formulário

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <Banner />
   <div class="main-container">
-    <h1>Monte seu burger</h1>
-    <p>Formulário</p>
+    <h1>Monte seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import BurgerForm from "../components/BurgerForm.vue"
+
 
 export default{
   name: 'Home',
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 
