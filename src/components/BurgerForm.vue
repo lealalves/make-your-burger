a7af717c7ff44a6d61a2eb834a68006dea75612e
lealalves <leealalves@hotmail.com>
2022-01-30 12:02:39 -0300

    pequenas alterações

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -96,7 +96,7 @@ export default {
             this.nome = ""
             this.carne = ""
             this.pao = ""
-            this.opcionais = ""
+            this.opcionais = []
 
 
         }
